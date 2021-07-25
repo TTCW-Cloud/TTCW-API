4175a0c61e6f21bfbca2aa74d08469fb0ceb3c4a
Xuwznln <1023025701@qq.com>
2021-07-25 21:02:43 +0800

    新Http服务器，新增测试页与短链接页

--- src/com/wznln/mc/api/FileReader.kt
+++ src/com/wznln/mc/api/FileReader.kt
@@ -4,7 +4,22 @@ import java.io.File
 
 object FileReader {
     @JvmStatic
-    fun getFileContent(path:String):String = File(path).readText(Charsets.UTF_8)
+    fun getFileContent(path:String):String = getFileContent(File(path))
     @JvmStatic
-    fun setFileContent(path:String,content:String) = File(path).writeText(content)
+    fun getFileContent(file:File):String = file.readText(Charsets.UTF_8)
+
+    @JvmStatic
+    fun setFileContent(path:String,content:String) = setFileContent(File(path),content)
+    @JvmStatic
+    fun setFileContent(file:File,content:String) = file.writeText(content,Charsets.UTF_8)
+
+    @JvmStatic
+    fun createFile(path:String) = createFile(File(path))
+    @JvmStatic
+    fun createFile(file:File) = file.createNewFile()
+
+    @JvmStatic
+    fun deleteFile(path:String) = deleteFile(File(path))
+    @JvmStatic
+    fun deleteFile(file:File) = file.delete()
 }
