773e3f7a62f322d94a188c55d19ef60eef66b397
Xuwznln <1023025701@qq.com>
2021-08-09 00:44:14 +0800

    added bungeecord support

---
+++ src/com/wznln/mc/api/BCmainlogic.java
@@ -0,0 +1,11 @@
+package com.wznln.mc.api;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BCmainlogic extends Plugin {
+    @Override
+    public void onEnable() { }
+
+    @Override
+    public void onDisable() { }
+}
